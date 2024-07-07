4c32727d7442f953ee821f19420bf73ce1202ce4
AhmedIsNotAvailable <muhammad.ahmed@camp2.tkxel.com>
2024-07-08 04:07:49 +0500

    Terraject

---
+++ variables.tf
@@ -0,0 +1,39 @@
+
+variable "region" {
+  type = string
+  default = "us-east-2"
+  description = "region set to: ohio"
+}
+
+
+variable "cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
+  description = "cidr block with 6k subnets"
+}
+
+variable "public_subnets" {
+  type = list(string)
+  default = [] 
+  description = "public nets from tfvars"
+}
+
+variable "private_subnets" {
+  type = list(string)
+  default = []
+  description = "public nets from tfvars"
+
+}
+
+variable "azs" {
+    type = list(string)
+    default = []
+    description = "azs in tfvars"
+  
+}
+
+
+variable "private_destination" {
+  type = string
+  default = "10.0.8.1/22"
+}
